be5c1fcf51581c92e0fc57f8a39194f483efb4c5
fdhlah30 <91524364+fdhlah30@users.noreply.github.com>
2021-09-29 20:11:01 +0700

    Add files via upload

---
+++ contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1> perkenalkan </h1>";
+        echo "Nama Saya Fadhilah Ardhiansyah
+              Saya Tinggal Di Daerah Bekasi
+              Hobi Saya Bermain Musik";
+    }
+}
